f87600871584c14ad9aeb905402065860379854b
warley.henrique <warley.henrique93@gmail.com>
2020-01-22 21:56:43 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require("./routes");
+const cors = require('cors');
 
 const app = express();
 
@@ -11,6 +12,7 @@ mongoose.connect('mongodb+srv://warleyhenrique:warleyhenrique@cluster0-nf5ir.gcp
 })
 
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
